dbbe0f2f1ef6f7e3d80613853256dcb707ba3542
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-09 22:58:39 -0400

    Create notificationModel.js

---
+++ src/models/notificationModel.js
@@ -0,0 +1,11 @@
+const notifications = [
+  {
+    id: 1,
+    userId: 1,
+    message: "Your order has been created",
+    type: "EMAIL",
+    status: "SENT"
+  }
+];
+
+module.exports = notifications;
