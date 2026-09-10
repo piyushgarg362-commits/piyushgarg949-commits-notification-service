16036d29ea3ad17a9692c89c5b257753f04e7e5c
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-09 22:59:16 -0400

    Create notificationController.js

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,38 @@
+const notifications =
+  require("../models/notificationModel");
+
+const getNotifications = (req, res) => {
+  res.json(notifications);
+};
+
+const createNotification = (req, res) => {
+  const {
+    userId,
+    message,
+    type
+  } = req.body;
+
+  if (!userId || !message || !type) {
+    return res.status(400).json({
+      message:
+        "userId, message and type are required"
+    });
+  }
+
+  const notification = {
+    id: notifications.length + 1,
+    userId,
+    message,
+    type,
+    status: "SENT"
+  };
+
+  notifications.push(notification);
+
+  res.status(201).json(notification);
+};
+
+module.exports = {
+  getNotifications,
+  createNotification
+};
