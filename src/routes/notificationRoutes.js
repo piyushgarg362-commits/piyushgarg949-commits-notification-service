5e3bb34a9fccb487de6142a43b882a91d79273ee
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-09 22:59:41 -0400

    Create notificationRoutes.js

---
+++ src/routes/notificationRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {
+  getNotifications,
+  createNotification
+} = require("../controllers/notificationController");
+
+const router = express.Router();
+
+router.get("/", getNotifications);
+
+router.post("/", createNotification);
+
+module.exports = router;
